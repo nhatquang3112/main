1a96bd80b91f03b805764cac6f0abc0952b70d2f
nhatquang3112 <trannhatquang3112@gmail.com>
2018-03-06 01:16:41 +0800

    Test Travis

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -15,7 +15,7 @@ import seedu.address.model.person.exceptions.DuplicatePersonException;
  * Adds a person to the address book.
  */
 public class AddCommand extends UndoableCommand {
-
+    // Test Travis
     public static final String COMMAND_WORD = "add";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
